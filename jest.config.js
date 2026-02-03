9df8402cb6b58466ebf9b75d873b6f6d290ed88e
iyanuloluwa-Miracle <iyanudina@gmail.com>
2026-02-03 15:33:03 +0100

    build: add Jest configuration for TypeScript tests
    - Use ts-jest preset for TypeScript support
    - Configure coverage collection
    - Set test environment to node
    - Match *.test.ts files in tests directory

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/database/migrations/**',
+    '!src/database/seeders/**',
+    '!src/config/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  verbose: true,
+  testTimeout: 10000
+};
